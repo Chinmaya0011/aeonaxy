146ead4030d2962005aa42cc1ffc7ee114a714e2
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-09 23:49:42 +0530

    updat

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-<div class="mx-auto mb-4  max-w-screen-sm justify-center items-center  text-center mt-10 font-bold m-4 p-4">
-
-<p class="text-blue-400">ROUTING FROMS</p>
-<h1 class="text-4xl font-bold mt-5 mb-8 ">Make higher quality connections</h1>
-<p class="text-1xl text-blue-300 mb-6 font-sans">Request information from website visitors or invitees and based on their response, route them to the right person or resource. Available on Professional plans and up.</p>
-<div class="space-x-4 mt-10">
-<button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded border" style={{ borderRadius: '30px' }}>Start for free</button>
-<button class="border border-black border-solid bg-white-100  hover:border-none text-gray-800 font-bold py-3 px-6 rounded" style={{ borderRadius: '30px' }}>Talk with sales</button>
-
-
-</div>
-</div>
-
-
-
-  )
+    <div className="mx-auto mb-4 max-w-screen-sm justify-center items-center text-center mt-10 font-bold m-4 p-4">
+      <p className="text-blue-400">ROUTING FROMS</p>
+      <h1 className="text-4xl font-bold mt-5 mb-8">Make higher quality connections</h1>
+      <p className="text-1xl text-blue-300 mb-6 font-sans">Request information from website visitors or invitees and based on their response, route them to the right person or resource. Available on Professional plans and up.</p>
+      <div className="space-x-4 mt-10">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded border" style={{ borderRadius: '30px' }}>Start for free</button>
+        <button className="border border-black border-solid bg-white-100 hover:border-none text-gray-800 font-bold py-3 px-6 rounded" style={{ borderRadius: '30px' }}>Talk with sales</button>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,24 +5,21 @@ const Video = () => {
   return (
     <div className='h-80vh justify-center items-center text-center' style={{backgroundImage: `url(${photo})`, backgroundSize: '100% 60%', backgroundRepeat: 'no-repeat'}}>
       <div className='p-4'>
-      
-      <p class="text-4xl text-white m-2 p-3 font-bold text-sky-blue">VIDEO</p>
-<h2 class="text-2xl m-2 p-2 font-bold text-white  w-80 mx-auto">See what you can do with Routin Forms</h2>
-
-    </div>
-    <div className="w-4/4 h-1/2 mx-auto relative lg:w-2/4 ">
-  <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage: `url(${photo})`}}>
-  <iframe
-      className="mx-auto inset-0 w-4/5 h-4.5/5 relative mt-4"
-      src="https://www.youtube.com/embed/B9Is7M-zh3Y?autoplay=0"
-      frameBorder="0"
-      allowFullScreen
-      width="80%"
-      height="90%"
-    ></iframe>
-  </div>
-</div>
-
+        <p className="text-4xl text-white m-2 p-3 font-bold text-sky-blue">VIDEO</p>
+        <h2 className="text-2xl m-2 p-2 font-bold text-white  w-80 mx-auto">See what you can do with Routing Forms</h2>
+      </div>
+      <div className="w-4/4 h-1/2 mx-auto relative lg:w-2/4">
+        <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage: `url(${photo})`}}>
+          <iframe
+            className="mx-auto inset-0 w-4/5 h-4.5/5 relative mt-4"
+            src="https://www.youtube.com/embed/B9Is7M-zh3Y?autoplay=0"
+            frameBorder="0"
+            allowFullScreen
+            width="80%"
+            height="90%"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
