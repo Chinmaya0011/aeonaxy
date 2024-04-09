4fa30b6e20a21c71332b95fd668531625c6456f0
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-09 12:59:39 +0530

    updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
+
